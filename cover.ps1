a4d0fef0d7bb2ba1ae7512a1497c0f5beae7219c
Josh Rickmar <jrick@conformal.com>
2016-02-02 16:14:26 -0500

    Fix coverage script.

--- cover.ps1
+++ cover.ps1
@@ -1,2 +1,2 @@
 & .\packages\OpenCover.4.6.166\tools\OpenCover.Console.exe -register:user "-filter:+[Paymetheus.Bitcoin]*" "-target:.\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe" "-targetargs:Paymetheus.Tests.Bitcoin.dll" "-targetdir:Paymetheus.Tests.Bitcoin\bin\Debug"
-& .\packages\ReportGenerator.2.4..0\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\coverage"
+& .\packages\ReportGenerator.2.4.0.0\tools\ReportGenerator.exe "-reports:results.xml" "-targetdir:.\coverage"
