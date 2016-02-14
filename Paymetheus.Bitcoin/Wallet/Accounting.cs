2cce4175e31b02f356086c1012614c2ba46267e8
Josh Rickmar <jrick@conformal.com>
2016-02-13 21:18:20 -0500

    Add a missing null argument check.

--- Paymetheus.Bitcoin/Wallet/Accounting.cs
+++ Paymetheus.Bitcoin/Wallet/Accounting.cs
@@ -1,12 +1,17 @@
 ﻿// Copyright (c) 2016 The btcsuite developers
 // Licensed under the ISC license.  See LICENSE file in the project root for full license information.
 
+using System;
+
 namespace Paymetheus.Bitcoin.Wallet
 {
     public static class Accounting
     {
         public static bool RelevantTransaction(WalletTransaction tx, Account account, out Amount debit, out Amount credit)
         {
+            if (tx == null)
+                throw new ArgumentNullException(nameof(tx));
+
             debit = 0;
             credit = 0;
             var relevant = false;
