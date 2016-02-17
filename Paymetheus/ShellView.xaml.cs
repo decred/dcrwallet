ba8ca3cb065d4f926deef30229f85a70cd2e7361
Josh Rickmar <jrick@companyzero.com>
2016-02-17 18:22:56 -0500

    Move license header to top of file.
    Somehow this got under the usings.

--- Paymetheus/ShellView.xaml.cs
+++ Paymetheus/ShellView.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) 2016 The btcsuite developers
+// Licensed under the ISC license.  See LICENSE file in the project root for full license information.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +16,6 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-// Copyright (c) 2016 The btcsuite developers
-// Licensed under the ISC license.  See LICENSE file in the project root for full license information.
 
 namespace Paymetheus
 {
