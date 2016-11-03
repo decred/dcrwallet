09ee04e42a9f78330688e3e95d80625281be386c
Jon Chappelow <jonathan.chappelow@gmail.com>
2016-11-03 10:39:16 -0700

    Handle flags.Error type assertion like in dcrd.

--- config.go
+++ config.go
@@ -323,9 +323,9 @@ func loadConfig() (*config, []string, error) {
 	preParser := flags.NewParser(&preCfg, flags.Default)
 	_, err := preParser.Parse()
 	if err != nil {
-		if e, ok := err.(*flags.Error); !ok || e.Type != flags.ErrHelp {
+		if e, ok := err.(*flags.Error); ok && e.Type != flags.ErrHelp {
 			preParser.WriteHelp(os.Stderr)
-		} else if e.Type == flags.ErrHelp {
+		} else if ok && e.Type == flags.ErrHelp {
 			preParser.WriteHelp(os.Stdout)
 		}
 		return loadConfigError(err)
