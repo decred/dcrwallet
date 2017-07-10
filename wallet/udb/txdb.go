005ed042385733b4795e92634224a36fdd260ddc
Alex Yocom-Piatt <ay-p@decred.org>
2017-07-10 20:49:10 +0000

    value not key

--- wallet/udb/txdb.go
+++ wallet/udb/txdb.go
@@ -790,8 +790,8 @@ func extractRawCreditIndex(k []byte) uint32 {
 	return byteOrder.Uint32(k[68:72])
 }
 
-func extractRawUnminedTx(k []byte) []byte {
-	return k[8:]
+func extractRawUnminedTx(v []byte) []byte {
+	return v[8:]
 }
 
 func extractRawUnminedCreditTxHash(k []byte) []byte {
