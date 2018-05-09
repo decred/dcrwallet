10f8b8da688832f5972a04b117c8c561522b412e
Josh Rickmar <jrick@companyzero.com>
2018-05-09 10:29:45 -0400

    Fix compilation of test after dcrd upgrade.

--- wallet/udb/cointype_test.go
+++ wallet/udb/cointype_test.go
@@ -51,9 +51,7 @@ func deriveChildAddress(accountExtKey *hdkeychain.ExtendedKey, branch, child uin
 }
 
 func equalExtKeys(k0, k1 *hdkeychain.ExtendedKey) bool {
-	s0, e0 := k0.String()
-	s1, e1 := k1.String()
-	return e0 == nil && e1 == nil && s0 == s1
+	return k0.String() == k1.String()
 }
 
 func TestCoinTypeUpgrade(t *testing.T) {
