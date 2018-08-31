ccafaf1633bfe45cd6261fcc3803f7a4af7abb9b
J Fixby <github@jfixby.com>
2018-08-31 17:22:44 +0200

    update helpers and fix TestListTransactions

--- rpctest/helpers.go
+++ rpctest/helpers.go
@@ -8,6 +8,7 @@ import (
 	"math"
 	"testing"
 	"time"
+	"strconv"
 
 	"github.com/decred/dcrd/chaincfg/chainhash"
 	"github.com/decred/dcrd/dcrjson"
@@ -18,11 +19,29 @@ import (
 )
 
 // Create a test chain with the desired number of mature coinbase outputs
-func generateTestChain(numToGenerate uint32, node *rpcclient.Client) {
+func generateTestChain(numToGenerate uint32, node *rpcclient.Client) error {
 	fmt.Printf("Generating %v blocks...\n", numToGenerate)
 	_, err := node.Generate(numToGenerate)
-	CheckTestSetupMalfunction(err)
+	if err != nil {
+		return err
+	}
 	fmt.Println("Block generation complete.")
+	return nil
+}
+
+// Waits for wallet to sync to the target height
+func syncWalletTo(rpcClient *rpcclient.Client, desiredHeight int64) (int64, error) {
+	var count int64 = 0
+	var err error = nil
+	for count != desiredHeight {
+		Sleep(1000)
+		count, err = rpcClient.GetBlockCount()
+		if err != nil {
+			return -1, err
+		}
+		fmt.Println("   sync to: " + strconv.FormatInt(count, 10))
+	}
+	return count, nil
 }
 
 func getMiningAddr(walletClient *rpcclient.Client) dcrutil.Address {
