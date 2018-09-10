decred/dcrwallet

commit 58cf828c2d186a74035225ddef7b50f098d0f675

Return errors for details of unknown transactions.
When a transaction is not yet known, lookups for details of the tx
must return an appropriate error.  This commit modifies the internal
TxDetails function to return non-nil errors rather than nil details
structs if the transaction is not recorded.

This is not a breaking change to consumers of the wallet module as the
udb package is a candidate for becoming internal and is not possible
to use in other projects as an internal walletdb interface is
necessary to create a udb.Store.

wallet/udb/txquery.go
@@ -170,9 +170,6 @@ func (s *Store) unminedTxDetails(ns walletdb.ReadBucket, txHash *chainhash.Hash,
 // TxDetails looks up all recorded details regarding a transaction with some
 // hash.  In case of a hash collision, the most recent transaction with a
 // matching hash is returned.
-//
-// Not finding a transaction with this hash is not an error.  In this case,
-// a nil TxDetails is returned.
 func (s *Store) TxDetails(ns walletdb.ReadBucket, txHash *chainhash.Hash) (*TxDetails, error) {
 	// First, check whether there exists an unmined transaction with this
 	// hash.  Use it if found.
@@ -185,8 +182,7 @@ func (s *Store) TxDetails(ns walletdb.ReadBucket, txHash *chainhash.Hash) (*TxDe
 	// hash, skip over to the newest and begin fetching all details.
 	k, v := latestTxRecord(ns, txHash[:])
 	if v == nil {
-		// not found
-		return nil, nil
+		return nil, errors.E(errors.NotFound)
 	}
 	return s.minedTxDetails(ns, txHash, k, v)
 }

rpc/legacyrpc/methods.go
@@ -1462,11 +1462,10 @@ func getTransaction(s *Server, icmd interface{}) (interface{}, error) {
 
 	// returns nil details when not found
 	txd, err := wallet.UnstableAPI(w).TxDetails(txHash)
-	if err != nil {
-		return nil, err
-	}
-	if txd == nil {
+	if errors.Is(errors.NotExist, err) {
 		return nil, rpcErrorf(dcrjson.ErrRPCNoTxInfo, "no information for transaction")
+	} else if err != nil {
+		return nil, err
 	}
 
 	_, tipHeight := w.MainChainTip()

wallet/wallet.go
@@ -2343,7 +2343,7 @@ func (w *Wallet) BlockInfo(blockID *BlockIdentifier) (*BlockInfo, error) {
 // TransactionSummary returns details about a recorded transaction that is
 // relevant to the wallet in some way.
 func (w *Wallet) TransactionSummary(txHash *chainhash.Hash) (txSummary *TransactionSummary, confs int32, blockHash *chainhash.Hash, err error) {
-	const op errors.Op = "wallet.TransactionSummary"
+	const opf = "wallet.TransactionSummary(%v)"
 	err = walletdb.View(w.db, func(dbtx walletdb.ReadTx) error {
 		ns := dbtx.ReadBucket(wtxmgrNamespaceKey)
 		_, tipHeight := w.TxStore.MainChainTip(ns)
@@ -2360,6 +2360,7 @@ func (w *Wallet) TransactionSummary(txHash *chainhash.Hash) (txSummary *Transact
 		return nil
 	})
 	if err != nil {
+		op := errors.Opf(opf, txHash)
 		return nil, 0, nil, errors.E(op, err)
 	}
 	return txSummary, confs, blockHash, nil
@@ -2868,7 +2869,7 @@ func (w *Wallet) ListUnspent(minconf, maxconf int32, addresses map[string]struct
 
 			details, err := w.TxStore.TxDetails(txmgrNs, &output.Hash)
 			if err != nil {
-				return errors.Errorf("Couldn't get credit details")
+				return err
 			}
 
 			// Outputs with fewer confirmations than the minimum or more
@@ -3798,13 +3799,10 @@ func (w *Wallet) SignTransaction(tx *wire.MsgTx, hashType txscript.SigHashType, 
 				prevHash := &txIn.PreviousOutPoint.Hash
 				prevIndex := txIn.PreviousOutPoint.Index
 				txDetails, err := w.TxStore.TxDetails(txmgrNs, prevHash)
-				if err != nil {
-					return errors.Errorf("Cannot query previous transaction "+
-						"details for %v: %v", txIn.PreviousOutPoint, err)
-				}
-				if txDetails == nil {
-					return errors.Errorf("%v not found",
-						txIn.PreviousOutPoint)
+				if errors.Is(errors.NotExist, err) {
+					return errors.Errorf("%v not found", &txIn.PreviousOutPoint)
+				} else if err != nil {
+					return err
 				}
 				prevOutScript = txDetails.MsgTx.TxOut[prevIndex].PkScript
 			}

commit 35021156dde9d13deaac21501c95d088b055eeda

Fix build.
58cf828c2 introduced usage of an undefined error kind.  This fixes it.

wallet/udb/txquery.go
@@ -182,7 +182,7 @@ func (s *Store) TxDetails(ns walletdb.ReadBucket, txHash *chainhash.Hash) (*TxDe
 	// hash, skip over to the newest and begin fetching all details.
 	k, v := latestTxRecord(ns, txHash[:])
 	if v == nil {
-		return nil, errors.E(errors.NotFound)
+		return nil, errors.E(errors.NotExist)
 	}
 	return s.minedTxDetails(ns, txHash, k, v)
 }