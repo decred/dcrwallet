566c13d1f06c79239597af8be4517ba6ef88c52b
githubsands <vracek@protonmail.com>
2018-10-08 09:59:03 -0700

    Fix test script to work on Darwin OS (#1299)

--- testmodules.sh
+++ testmodules.sh
@@ -9,7 +9,7 @@ fi
 
 ROOTPATH=$($GO list -m -f {{.Dir}} 2>/dev/null)
 ROOTPATHPATTERN=$(echo $ROOTPATH | sed 's/\\/\\\\/g' | sed 's/\//\\\//g')
-MODPATHS=$($GO list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN" | sed -e "s/^$ROOTPATHPATTERN//" -e 's/^\\\|\///')
+MODPATHS=$($GO list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN" | sed -e "s/^$ROOTPATHPATTERN//" -e 's/^\\//' -e 's/^\///')
 MODPATHS=". $MODPATHS"
 
 tests () {
