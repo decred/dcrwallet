8d50fbd51852188e5ca5339003dfe5a84b19ae20
Josh Rickmar <jrick@companyzero.com>
2019-05-06 14:51:10 +0000

    Fix cursor indexes to derive next address.
    The cursor index was not being set to the correct value, causing addresses to be
    returned multiple times after any block or unmined transaction was received.
    This has been fixed and tests have been added to ensure the behavior remains
    correct, as this is not the first time address reuse has become a problem.
    
    Backport of a29d7c7231aa027905d2acfb75a8fda5f22ff9a6.

---
+++ wallet/network_test.go
@@ -0,0 +1,37 @@
+// Copyright (c) 2019 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package wallet
+
+import (
+	"context"
+
+	"github.com/decred/dcrd/chaincfg/chainhash"
+	"github.com/decred/dcrd/dcrutil"
+	"github.com/decred/dcrd/gcs"
+	"github.com/decred/dcrd/wire"
+)
+
+// mockNetwork implements all methods of NetworkBackend, returning zero values
+// without error.  It may be embedded in a struct to create another
+// NetworkBackend which dispatches to particular implementations of the methods.
+type mockNetwork struct{}
+
+func (mockNetwork) GetBlocks(ctx context.Context, blockHashes []*chainhash.Hash) ([]*wire.MsgBlock, error) {
+	return nil, nil
+}
+func (mockNetwork) GetCFilters(ctx context.Context, blockHashes []*chainhash.Hash) ([]*gcs.Filter, error) {
+	return nil, nil
+}
+func (mockNetwork) GetHeaders(ctx context.Context, blockLocators []*chainhash.Hash, hashStop *chainhash.Hash) ([]*wire.BlockHeader, error) {
+	return nil, nil
+}
+func (mockNetwork) PublishTransactions(ctx context.Context, txs ...*wire.MsgTx) error { return nil }
+func (mockNetwork) LoadTxFilter(ctx context.Context, reload bool, addrs []dcrutil.Address, outpoints []wire.OutPoint) error {
+	return nil
+}
+func (mockNetwork) Rescan(ctx context.Context, blocks []chainhash.Hash, r RescanSaver) error {
+	return nil
+}
+func (mockNetwork) StakeDifficulty(ctx context.Context) (dcrutil.Amount, error) { return 0, nil }
