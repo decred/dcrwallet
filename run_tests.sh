7f20c1722cf2ed4ba3f010c0326122bc0dcd6469
David Hill <dhill@mindcry.org>
2019-08-01 15:35:26 -0400

    travis: Use more portable module ver stripping.

--- run_tests.sh
+++ run_tests.sh
@@ -7,7 +7,7 @@ go version
 GOTESTFLAGS='-short'
 ROOTPATH=$(go list -m -f {{.Dir}} 2>/dev/null)
 ROOTPATHPATTERN=$(echo $ROOTPATH | sed 's/\\/\\\\/g' | sed 's/\//\\\//g')
-MODPATHS=$(go list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN" | sed -e "s/^$ROOTPATHPATTERN//" -e 's/^\\//' -e 's/^\///')
+MODPATHS=$(go list -m -f {{.Dir}} all 2>/dev/null | grep "^$ROOTPATHPATTERN" | sed -E -e "s/^$ROOTPATHPATTERN//" -e 's,^/,,' -e 's,/v[0-9]+$,,')
 MODPATHS=". $MODPATHS"
 
 for m in $MODPATHS; do
