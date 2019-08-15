fcdae17febb640c834b5993fb94f9d8fb42ab624
Cédric Félizard <cedric@felizard.fr>
2019-08-15 12:37:15 +0000

    Fix typo
    Rename old reference to btcwallet.

--- rpc/jsonrpc/server.go
+++ rpc/jsonrpc/server.go
@@ -86,7 +86,7 @@ type handler struct {
 
 // jsonAuthFail sends a message back to the client if the http auth is rejected.
 func jsonAuthFail(w http.ResponseWriter) {
-	w.Header().Add("WWW-Authenticate", `Basic realm="btcwallet RPC"`)
+	w.Header().Add("WWW-Authenticate", `Basic realm="dcrwallet RPC"`)
 	http.Error(w, "401 Unauthorized.", http.StatusUnauthorized)
 }
 
