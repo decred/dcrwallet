30c717b394eea67a1cf840d56e8084a6fb517f27
Josh Rickmar <jrick@companyzero.com>
2020-02-26 09:26:46 -0500

    Unify files dealing with TLS signature schemes
    The handling was split into multiple files compiled with different
    build tags depending on the Go version, which allowed for use of
    Ed25519 as the signature scheme for dcrwallet RPC certificates while
    still supporting builds on Go 1.12.  This is no longer necessary now
    that pre-1.13 Go support has been removed.

--- internal/cfgutil/curve.go
+++ internal/cfgutil/curve.go
@@ -4,7 +4,13 @@
 
 package cfgutil
 
-import "crypto/elliptic"
+import (
+	"crypto/elliptic"
+	"time"
+
+	"decred.org/dcrwallet/errors"
+	"github.com/decred/dcrd/certgen"
+)
 
 // CurveID specifies a recognized curve through a constant value.
 type CurveID int
@@ -15,7 +21,10 @@ const (
 	CurveP256
 	CurveP384
 	CurveP521
-	afterECDSA
+	Ed25519
+
+	// PreferredCurve is the curve that should be used as the application default.
+	PreferredCurve = Ed25519
 )
 
 // CurveFlag describes a curve and implements the flags.Marshaler and
@@ -44,4 +53,52 @@ func (f *CurveFlag) ECDSACurve() (elliptic.Curve, bool) {
 	default:
 		return nil, false
 	}
+}
+
+// MarshalFlag satisfies the flags.Marshaler interface.
+func (f *CurveFlag) MarshalFlag() (name string, err error) {
+	switch f.curveID {
+	case CurveP224:
+		name = "P-224"
+	case CurveP256:
+		name = "P-256"
+	case CurveP384:
+		name = "P-384"
+	case CurveP521:
+		name = "P-521"
+	case Ed25519:
+		name = "Ed25519"
+	default:
+		err = errors.Errorf("unknown curve ID %v", int(f.curveID))
+	}
+	return
+}
+
+// UnmarshalFlag satisfies the flags.Unmarshaler interface.
+func (f *CurveFlag) UnmarshalFlag(value string) error {
+	switch value {
+	case "P-224":
+		f.curveID = CurveP224
+	case "P-256":
+		f.curveID = CurveP256
+	case "P-384":
+		f.curveID = CurveP384
+	case "P-521":
+		f.curveID = CurveP521
+	case "Ed25519":
+		f.curveID = Ed25519
+	default:
+		return errors.Errorf("unrecognized curve %v", value)
+	}
+	return nil
+}
+
+func (f *CurveFlag) CertGen(org string, validUntil time.Time, extraHosts []string) (cert, key []byte, err error) {
+	if ec, ok := f.ECDSACurve(); ok {
+		return certgen.NewTLSCertPair(ec, org, validUntil, extraHosts)
+	}
+	if f.curveID == Ed25519 {
+		return certgen.NewEd25519TLSCertPair(org, validUntil, extraHosts)
+	}
+	return nil, nil, errors.New("unknown curve ID")
 }
