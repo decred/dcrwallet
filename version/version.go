82f59828803cef2e0080db63ddfb43a02e568788
Josh Rickmar <jrick@companyzero.com>
2023-06-09 16:14:11 +0000

    Update version prerelease and metadata for release-v1.8 branch

--- version/version.go
+++ version/version.go
@@ -29,12 +29,12 @@ const Integer = 1000000*Major + 10000*Minor + 100*Patch
 // so it can be modified at link time (e.g.
 // `-ldflags "-X decred.org/dcrwallet/v3/version.PreRelease=rc1"`).
 // It must only contain characters from the semantic version alphabet.
-var PreRelease = "pre"
+var PreRelease = ""
 
 // BuildMetadata defines additional build metadata.  It is modified at link time
 // for official releases.  It must only contain characters from the semantic
 // version alphabet.
-var BuildMetadata = ""
+var BuildMetadata = "release.local"
 
 func init() {
 	if BuildMetadata == "" {
