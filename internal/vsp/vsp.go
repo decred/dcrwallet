09bb3c2c509511667599af56b28552f5a5ce73a2
jholdstock <jholdstock@decred.org>
2023-09-24 09:37:01 +0100

    vsp: Update to latest types/client.

--- internal/vsp/vsp.go
+++ internal/vsp/vsp.go
@@ -22,7 +22,7 @@ import (
 	"github.com/decred/dcrd/txscript/v4/stdaddr"
 	"github.com/decred/dcrd/wire"
 	"github.com/decred/slog"
-	vspd "github.com/decred/vspd/client/v2"
+	vspd "github.com/decred/vspd/client/v3"
 )
 
 type DialFunc func(ctx context.Context, network, addr string) (net.Conn, error)
