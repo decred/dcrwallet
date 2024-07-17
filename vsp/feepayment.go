581e41c01592793d28fcfd1cfddbfb5cac3fce53
jholdstock <jholdstock@decred.org>
2024-07-17 10:28:32 +0100

    Update to latest vspd modules.
    types/v3 removes the deprecated "revoked" field from /vspinfo response
    (not used by dcrwallet).

--- vsp/feepayment.go
+++ vsp/feepayment.go
@@ -20,7 +20,7 @@ import (
 	"github.com/decred/dcrd/dcrutil/v4"
 	"github.com/decred/dcrd/txscript/v4/stdaddr"
 	"github.com/decred/dcrd/wire"
-	"github.com/decred/vspd/types/v2"
+	"github.com/decred/vspd/types/v3"
 )
 
 var (
