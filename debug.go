9c605326ed1907eb7d2857da17485c163fb94ed3
Josh Rickmar <jrick@companyzero.com>
2024-09-11 18:51:03 +0000

    Use Go 1.23 default GODEBUG

---
+++ debug.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+//go:build go1.23
+
+//go:debug default=go1.23
+
+package main
