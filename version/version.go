b47c4de7ebdb973fd0b86c0588a610d20b52b6eb
Josh Rickmar <jrick@companyzero.com>
2025-02-20 18:51:07 +0000

    [release-v2.0] version: Crank to 2.0.6

--- version/version.go
+++ version/version.go
@@ -19,7 +19,7 @@ const semverAlphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst
 const (
 	Major = 2
 	Minor = 0
-	Patch = 5
+	Patch = 6
 )
 
 // Integer is an integer encoding of the major.minor.patch version.
