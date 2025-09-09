209ba35299e8927a7beec204d67c7053a4ce317f
jholdstock <jholdstock@decred.org>
2025-09-09 09:20:53 +0100

    Remove unused/unneccessary code.

--- rpcserver.go
+++ rpcserver.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2015 The btcsuite developers
-// Copyright (c) 2015-2024 The Decred developers
+// Copyright (c) 2015-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -338,7 +338,6 @@ func startRPCServers(ctx context.Context, walletLoader *loader.Loader) (*grpc.Se
 			rpcserver.StartDecodeMessageService(server, activeNet.Params)
 			rpcserver.StartMessageVerificationService(server, activeNet.Params)
 			for _, lis := range listeners {
-				lis := lis
 				go func() {
 					laddr := lis.Addr().String()
 					grpcAddrNotifier.notify(laddr)
