1e8b065a0fdf6299877d40049c598e46314d1981
jholdstock <jholdstock@decred.org>
2025-09-12 11:21:43 +0100

    Remove unused func UnspentOutputs.
    This func has no known callers, might as well remove it while we are in
    an unreleased version bump.

--- wallet/utxos.go
+++ wallet/utxos.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2021 The Decred developers
+// Copyright (c) 2016-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -14,7 +14,6 @@ import (
 	"decred.org/dcrwallet/v5/wallet/udb"
 	"decred.org/dcrwallet/v5/wallet/walletdb"
 	"github.com/decred/dcrd/dcrutil/v4"
-	"github.com/decred/dcrd/txscript/v4/stdscript"
 	"github.com/decred/dcrd/wire"
 )
 
@@ -23,86 +22,6 @@ import (
 type OutputSelectionPolicy struct {
 	Account               uint32
 	RequiredConfirmations int32
-}
-
-func (p *OutputSelectionPolicy) meetsRequiredConfs(txHeight, curHeight int32) bool {
-	return confirmed(p.RequiredConfirmations, txHeight, curHeight)
-}
-
-// UnspentOutputs fetches all unspent outputs from the wallet that match rules
-// described in the passed policy.
-func (w *Wallet) UnspentOutputs(ctx context.Context, policy OutputSelectionPolicy) ([]*TransactionOutput, error) {
-	const op errors.Op = "wallet.UnspentOutputs"
-
-	defer w.lockedOutpointMu.Unlock()
-	w.lockedOutpointMu.Lock()
-
-	var outputResults []*TransactionOutput
-	err := walletdb.View(ctx, w.db, func(dbtx walletdb.ReadTx) error {
-		addrmgrNs := dbtx.ReadBucket(waddrmgrNamespaceKey)
-
-		_, tipHeight := w.txStore.MainChainTip(dbtx)
-
-		// TODO: actually stream outputs from the db instead of fetching
-		// all of them at once.
-		outputs, err := w.txStore.UnspentOutputs(dbtx)
-		if err != nil {
-			return err
-		}
-
-		for _, output := range outputs {
-			// Ignore outputs that haven't reached the required
-			// number of confirmations.
-			if !policy.meetsRequiredConfs(output.Height, tipHeight) {
-				continue
-			}
-
-			// Ignore outputs that are not controlled by the account.
-			_, addrs := stdscript.ExtractAddrs(scriptVersionAssumed, output.PkScript, w.chainParams)
-			if len(addrs) == 0 {
-				// Cannot determine which account this belongs
-				// to without a valid address.  TODO: Fix this
-				// by saving outputs per account, or accounts
-				// per output.
-				continue
-			}
-			outputAcct, err := w.manager.AddrAccount(addrmgrNs, addrs[0])
-			if err != nil {
-				return err
-			}
-			if outputAcct != policy.Account {
-				continue
-			}
-
-			// Stakebase isn't exposed by wtxmgr so those will be
-			// OutputKindNormal for now.
-			outputSource := OutputKindNormal
-			if output.FromCoinBase {
-				outputSource = OutputKindCoinbase
-			}
-
-			result := &TransactionOutput{
-				OutPoint: output.OutPoint,
-				Output: wire.TxOut{
-					Value: int64(output.Amount),
-					// TODO: version is bogus but there is
-					// only version 0 at time of writing.
-					Version:  0,
-					PkScript: output.PkScript,
-				},
-				OutputKind:      outputSource,
-				ContainingBlock: BlockIdentity(output.Block),
-				ReceiveTime:     output.Received,
-			}
-			outputResults = append(outputResults, result)
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, errors.E(op, err)
-	}
-	return outputResults, nil
 }
 
 // SelectInputs selects transaction inputs to redeem unspent outputs stored in
