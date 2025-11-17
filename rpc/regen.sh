be24e23c7214f8a0ee0ea65f19af83705ce872d5
Josh Rickmar <jrick@companyzero.com>
2025-11-17 18:50:32 +0000

    Use go.mod tool directive
    Use shell wrappers to invoke protobuf plugin tools via 'go tool'.
    These must still be executables found in PATH by regen.sh.
    
    The tool directive is available since Go 1.24.
    
    Updates to the latest version of the protobuf module to use the latest
    tools.

--- rpc/regen.sh
+++ rpc/regen.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-build_tools() {
-    mkdir -p bin
-    export GOBIN=$PWD/bin
-    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
-    go install google.golang.org/protobuf/cmd/protoc-gen-go
-}
-
 generate() {
     protoc -I. api.proto --go_out=walletrpc --go-grpc_out=walletrpc \
         --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative
@@ -15,5 +8,4 @@ generate() {
     [ -n "$UID" ] && chown -R $UID . 2>/dev/null || return 0
 }
 
-(cd tools && build_tools)
 PATH=$PWD/tools/bin:$PATH generate
