69ef8cfe4e860a67c637b0b8b75ea90e79674c8c
Jamie Holdstock <jholdstock@decred.org>
2025-11-25 14:14:07 +0000

    udb: Close DB before deleting file in tests.

--- wallet/udb/common_test.go
+++ wallet/udb/common_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2014 The btcsuite developers
-// Copyright (c) 2015-2024 The Decred developers
+// Copyright (c) 2015-2025 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
@@ -89,8 +89,8 @@ func cloneDB(ctx context.Context, t *testing.T, cloneName string) (walletdb.DB, 
 	}
 
 	t.Cleanup(func() {
-		os.Remove(cloneName)
 		db.Close()
+		os.Remove(cloneName)
 	})
 
 	return db, mgr, txStore, err
